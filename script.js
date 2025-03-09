27e2ef69c663571567ed164b9a52d4fcee2e6007
M-Lenvik <lenvik.marie@gmail.com>
2025-03-09 21:45:09 +0100

    fix: svg animation and css on page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-undef
+/** Gäller initialerna i namnet */
 const tl = new gsap.timeline();
 const letters = [
     "#letter_m",  
@@ -8,16 +8,28 @@ const letters = [
 letters.forEach((letter) => {
   tl.to(letter, { 
     duration: 1.5, 
-    fill: "#C26700",
+    fill: "#E97C00",
     transformOrigin: "center center",
     rotation: 360
   });
 });
 
-// eslint-disable-next-line no-undef
-gsap.to('#logo', { 
+/** Gäller cirkeln */
+gsap.set("#cls-2-group_initials", { fill: "#000" }); //Sätter initial färg  
+gsap.to("#cls-2-group_initials", { 
+  duration: 5, 
+  fill: "#E97C00",
   transformOrigin: "50% 50%", 
   rotation: 360, 
-  duration: 3, 
+  repeat: -1, //Upprepa oändligt
+  repeatDelay: 5
+});
+
+gsap.to('#cls-2-tagg-group', { 
+  transformOrigin: "50% 50%", 
+  rotation: "-=360", 
+  duration: 10, 
   ease: "linear",
+  repeat: -1,
+  yoyo: true,
 });
